53568f0ebc42c1dfb02649f8bfd19bacd07cdc6c
Sender60 <butkov60@gmail.com>
2023-09-20 01:11:25 +0300

    Небольшая переименовка файлов и добавление fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/app.scss';
-import Header from './components/header/header';
-import Categories from './components/сategories/сategories';
-import Sort from './components/sort/sort';
-import PizzaBlock from './components/pizza-block/pizza-block';
-import pizzas from './assets/pizzas.json';
+import Header from './components/header';
+import Categories from './components/сategories';
+import Sort from './components/sort';
+import PizzaBlock from './components/pizza-block';
+// import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://64d95baee947d30a260a13b5.mockapi.io/Items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,8 +28,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock {...obj} />
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
         </div>
--- src/components/sort/index.jsx
+++ src/components/sort/index.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Sort() {
+  const [open, setOpen] = useState(false);
+
+  const sort = ['популярности', 'цене', 'алфавиту'];
+  const [selected, setSelected] = useState(0);
+
+  const onClickListItem = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -16,15 +26,22 @@ export default function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sort[selected]}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sort.map((value, i) => (
+              <li
+                key={value}
+                onClick={() => onClickListItem(i)}
+                className={selected === i ? 'active' : ''}>
+                {value}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
